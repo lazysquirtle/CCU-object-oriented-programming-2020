669019cc803348479c2af1169f08842a7acbc3eb
Felix Sung <felixsung019@gmail.com>
2023-01-13 00:46:13 +0800

    Update OOP_Hw01.cpp
    Create a routing table and implement BFS

--- OOP_Hw01/OOP_Hw01.cpp
+++ OOP_Hw01/OOP_Hw01.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 vector <vector <unsigned int >>VectorStorePath ;
 
-class NODE {																													//物件NODE
+//建立節點
+class NODE {											
 public:
-	NODE();																														//建構子
-	unsigned int  SendTo(unsigned int Dst,unsigned int NodeMinus);			//一堆函式，功能如名字
+	NODE();											//建構子
+	unsigned int  SendTo(unsigned int Dst,unsigned int NodeMinus);			
 	unsigned int  GetValue(void);
 	unsigned int  GetDistance(void);
 	void StoreNeighbor(unsigned int NeighborNode);
@@ -20,7 +21,7 @@ public:
 	vector<unsigned int>* GetNeighbor(void);
 	//void PrintRoutingTable(unsigned int Node);
 
-private:																															//物件基本資料
+private:											//物件基本資料
 	unsigned int ID;
 	unsigned int Distance;
 	vector <unsigned int > Neighbor;
@@ -28,11 +29,12 @@ private:																															//物件基本資料
 };
 
 NODE::NODE() {
-	ID = 0;																														//ID
-	Distance = 0;																											//距離某一個node的距離
+	ID = 0;											//ID
+	Distance = 0;										//距離某一個node的距離
 }
 
-void NODE::StoreNeighbor(unsigned int NeighborNode) {					//這個node和那些node是鄰居
+//這個node和那些node是鄰居
+void NODE::StoreNeighbor(unsigned int NeighborNode) {					
 	Neighbor.push_back(NeighborNode);
 }
 
@@ -52,13 +54,14 @@ unsigned int NODE::GetDistance() {
 	return Distance;
 }
 
-vector<unsigned int>* NODE::GetNeighbor() {						//得到鄰居有哪些
+//得到鄰居有哪些
+vector<unsigned int>* NODE::GetNeighbor() {						
 	vector <unsigned int >* VectorOut = new vector<unsigned int>;
 	*VectorOut = Neighbor;
 	return VectorOut;
 }
 
-unsigned int  NODE::SendTo(unsigned int Dst,unsigned int NodeMinus) {			//老師給的函式
+unsigned int  NODE::SendTo(unsigned int Dst,unsigned int NodeMinus) {			
 	unsigned int result = 0;
 	for (int i = 0; i < NodeMinus; i++) {
 		if (RoutingTable[i][0] == Dst)
@@ -76,39 +79,40 @@ void NODE::PrintRoutingTable(unsigned int Node) {
 	cout << endl << endl;
 }
 */
-void NODE::MakeARoutingTable(NODE* ptr,unsigned int Node) {				//建table，table有 -終點--下個節點-2欄
-			
+
+//建routing table, |終點|下個節點|2欄
+void NODE::MakeARoutingTable(NODE* ptr,unsigned int Node) {					
 	vector <vector <unsigned int >>OuterVector;									
 	for (int i = 0; i < Node; i++) {
 
-		if ((ptr + i)->GetValue() == ID)																				//不用建終點為自己的那列
+		if ((ptr + i)->GetValue() == ID)							//不用建終點為自己的那列
 			continue;
 
 		vector <unsigned int>InnerVec;
 		unsigned int Dis = (ptr + i)->GetDistance();
 
-		if ((ptr + i)->Distance == 1) {																					//距離為1(鄰居)就直接存到next
+		if ((ptr + i)->Distance == 1) {								//距離為1(鄰居)就直接存到next
 			InnerVec.push_back(i);
 			InnerVec.push_back(i);
 			OuterVector.push_back(InnerVec);
 		}
-		else {																																//距離不為1就去找鄰居距離為1的
+		else {											//距離不為1就去找鄰居距離為1的
 			vector <unsigned int >*fuck;
 			vector <unsigned int >::iterator iter;
-			fuck = (ptr + i)->GetNeighbor();																			//fuck 指向一排鄰居
+			fuck = (ptr + i)->GetNeighbor();						//fuck 指向一排鄰居
 
 
 			for (iter = (*fuck).begin(); iter != (*fuck).end(); ) {
-				if ((ptr + *iter)->Distance == 1) {																	//如果找到鄰居距離為1的了
+				if ((ptr + *iter)->Distance == 1) {					//如果找到鄰居距離為1的了
 					InnerVec.push_back(i);
 					InnerVec.push_back(*iter);
-					OuterVector.push_back(InnerVec);															//存進去
+					OuterVector.push_back(InnerVec);				//存進去
 					break;
 				}
 				else {
-					if ((ptr + *iter)->Distance == Dis - 1) {														//如果都找不到
-						fuck = (ptr + *iter)->GetNeighbor();														//找最小ID的那個
-						iter = (*fuck).begin();																					//重設鄰居(鄰居變成最小ID的那個)
+					if ((ptr + *iter)->Distance == Dis - 1) {			//如果都找不到
+						fuck = (ptr + *iter)->GetNeighbor();			//找最小ID的那個
+						iter = (*fuck).begin();					//重設鄰居(鄰居變成最小ID的那個)
 						Dis = Dis - 1;
 						continue;
 					}
@@ -130,7 +134,9 @@ NODE* CreateObject(unsigned int NumOfNode);
 int main() {
 
 	NODE* ptr = NULL;
-	unsigned int NodeNumber = 0, PathNumber = 0;					//一直輸入			
+	unsigned int NodeNumber = 0, PathNumber = 0;
+	
+	//輸入節點和路徑
 	cin >> NodeNumber  >>PathNumber ;
 
 	for (unsigned int i = 0 ; i < PathNumber ; i++) {											
@@ -142,12 +148,13 @@ int main() {
 		VectorStore.push_back(End);
 		VectorStorePath.push_back(VectorStore);
 	}
-
-	ptr = CreateObject(NodeNumber) ;						//根據輸入建立物件、網路
+	
+	//根據輸入建立物件、網路
+	ptr = CreateObject(NodeNumber) ;						
 	ContructNodeNeighbor(ptr, NodeNumber, PathNumber) ;
 	
 	for (unsigned int i = 0; i < NodeNumber; i++) {
-		BFS(i, NodeNumber, ptr);																			//做完BFS後建立TABLE
+		BFS(i, NodeNumber, ptr);				//做完BFS後建立TABLE
 		(ptr + i)->MakeARoutingTable(ptr,NodeNumber);
 		//(ptr + i)->PrintRoutingTable(NodeNumber);
 	}
@@ -156,7 +163,7 @@ int main() {
 	cin >> PathYouWant ;
 	
 	vector <vector<unsigned int>>VecSrcAndDst ;
-	for (unsigned int i = 0; i < PathYouWant; i++) {				//輸入你要的path
+	for (unsigned int i = 0; i < PathYouWant; i++) {		//輸入你要的path
 		
 		unsigned int No = 0, Start = 0, End = 0;
 		cin >> No >> Start >> End;
@@ -167,13 +174,13 @@ int main() {
 		VecSrcAndDst.push_back(Vec);
 	}
 
-	for (unsigned int i = 0; i < PathYouWant ; i++) {				//找路徑用的
-		unsigned int Cur_ID = VecSrcAndDst[ i ][1];				//Source
-		unsigned int Dst_ID = VecSrcAndDst[ i ][2];				//Destination
+	for (unsigned int i = 0; i < PathYouWant ; i++) {		//找路徑用的
+		unsigned int Cur_ID = VecSrcAndDst[ i ][1];		//Source
+		unsigned int Dst_ID = VecSrcAndDst[ i ][2];		//Destination
 		cout << i << '\t';
-		while (Cur_ID != Dst_ID) {																			//Source!=Destination
+		while (Cur_ID != Dst_ID) {				//Source!=Destination
 			cout << Cur_ID << '\t' ;
-			Cur_ID = (ptr + Cur_ID)->SendTo(Dst_ID, NodeNumber-1);		//找阿
+			Cur_ID = (ptr + Cur_ID)->SendTo(Dst_ID, NodeNumber-1);		
 		}
 		cout << Cur_ID << endl;
 	}
@@ -181,16 +188,16 @@ int main() {
 	return 0;
 }
 
-NODE*  CreateObject(unsigned int NumOfNode) {						//create the object  NumberOfNode 
-
-	NODE *PTR = new NODE[NumOfNode];						//ptr point to the six node's head 
+//create the object NumberOfNode
+NODE*  CreateObject(unsigned int NumOfNode) {						 
+	NODE *PTR = new NODE[NumOfNode];						
 	for (unsigned int i = 0; i < NumOfNode; i++)
 		(PTR + i)->SetValue(i);
 	return PTR;
 }
 
-void ContructNodeNeighbor(NODE* ptr,unsigned int Node,unsigned int Path) {		//record node neighbor  
-	
+//record node neighbor
+void ContructNodeNeighbor(NODE* ptr,unsigned int Node,unsigned int Path) {
 	for (int i = 0; i < Path; i++) {
 		for (unsigned int j = 0; j < Node; j++) {
 			if (VectorStorePath[i][1] == (ptr + j)->GetValue())
@@ -200,12 +207,11 @@ void ContructNodeNeighbor(NODE* ptr,unsigned int Node,unsigned int Path) {		//re
 			if (VectorStorePath[i][2] == (ptr + j)->GetValue())
 				(ptr + j)->StoreNeighbor(VectorStorePath[i][1]);
 		}
-
 	}
 }
 
-void BFS(unsigned int Source,unsigned  int Node, NODE* ptr) {														//BFS
-	
+//BFS
+void BFS(unsigned int Source,unsigned  int Node, NODE* ptr) {														
 	for(unsigned int i = 0 ; i < Node; i++)
 		(ptr + i)->SetDistance(0);
 
